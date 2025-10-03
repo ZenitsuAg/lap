c1f144cc2538065345e914d0da9479e02755e994
Fred Robins <fredrobins619@outlook.com>
2025-10-03 22:43:27 +0000

    Redesigned the Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,24 +2,26 @@ import Agrod from "../../images/AgroD.svg";
 
 const Header = () => {
     return (
-        <nav className="border-b shadow-emerald-800 flex justify-between p-3 px-3.5 max-sm:justify-center shadow fixed w-full backdrop-blur-lg">
-            <div>
-                <a href="#home" className="flex gap-1 justify-center">
-                    <img src={Agrod} alt="lol" className="w-20" />
-                </a>
-            </div>
-            <div className="flex gap-4 ">
-                <button className="cursor-pointer hover:text-emerald-700 duration-150 hover:scale-125">
-                    Farmers
-                </button>
-                <button className="cursor-pointer hover:text-emerald-700 duration-150 hover:scale-125">
-                    Investors
-                </button>
-                <button className="border-b hover:border-b-emerald-700 duration-150 hover:scale-125 hover:text-emerald-700">
-                    Connect Wallet
-                </button>
-            </div>
-        </nav>
+        <div className="pt-4 mt-10 flex justify-center items-center fixed w-full">
+            <nav className="border border-transparent max-sm:w-[350px] lg:px-10 shadow rounded-4xl flex gap-60 justify-between p-3 px-3.5 max-sm:justify-center   backdrop-blur-lg">
+                <div>
+                    <a href="#home" className="flex gap-1 justify-center">
+                        <img src={Agrod} alt="lol" className="w-20" />
+                    </a>
+                </div>
+                <div className="flex gap-4 max-md:hidden">
+                    <button className="cursor-pointer hover:text-emerald-700 duration-150 hover:scale-125">
+                        Farmers
+                    </button>
+                    <button className="cursor-pointer hover:text-emerald-700 duration-150 hover:scale-125">
+                        Investors
+                    </button>
+                    <button className="border-b hover:border-b-emerald-700 duration-150 hover:scale-125 hover:text-emerald-700">
+                        Connect Wallet
+                    </button>
+                </div>
+            </nav>
+        </div>
     );
 };
 
